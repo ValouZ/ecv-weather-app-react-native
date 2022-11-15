5abde23dd19ad6ae85de8f7bda859e85ca73c25a
ValouZ <57949313+ValouZ@users.noreply.github.com>
2022-11-15 18:22:34 +0100

    end weatherinfo

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -20,6 +20,42 @@ const WeatherInfo = ({ weatherData }) => {
         <Text style={styles.currentTemp}>{temp} °C</Text>
       </View>
       <Text style={styles.description}>{description}</Text>
+      <View style={styles.extraInfo}>
+        <View style={styles.info}>
+          {/* <Image style={styles.smallIcon} source={{uri:}} /> */}
+          <Text style={styles.infoText}>{feels_like} °C</Text>
+          <Text style={styles.infoText}>Feels like</Text>
+        </View>
+        <View style={styles.info}>
+          {/* <Image style={styles.smallIcon} source={{uri:}} /> */}
+          <Text style={styles.infoText}>{humidity} %</Text>
+          <Text style={styles.infoText}>Humidity</Text>
+        </View>
+      </View>
+      <View style={styles.extraInfo}>
+        <View style={styles.info}>
+          {/* <Image style={styles.smallIcon} source={{uri:}} /> */}
+          <Text style={styles.infoText}>{visibility}</Text>
+          <Text style={styles.infoText}>Visibility</Text>
+        </View>
+        <View style={styles.info}>
+          {/* <Image style={styles.smallIcon} source={{uri:}} /> */}
+          <Text style={styles.infoText}>{speed} m/s</Text>
+          <Text style={styles.infoText}>Wind Speed</Text>
+        </View>
+      </View>
+      <View style={styles.extraInfo}>
+        <View style={styles.info}>
+          {/* <Image style={styles.smallIcon} source={{uri:}} /> */}
+          <Text style={styles.infoText}>{new Date(sunrise*1000).toLocaleString()}</Text>
+          <Text style={styles.infoText}>Sunrise</Text>
+        </View>
+        <View style={styles.info}>
+          {/* <Image style={styles.smallIcon} source={{uri:}} /> */}
+          <Text style={styles.infoText}>{new Date(sunset*1000).toLocaleString()}</Text>
+          <Text style={styles.infoText}>Sunset</Text>
+        </View>
+      </View>
     </SafeAreaView>
   );
 };
@@ -58,5 +94,27 @@ const styles = StyleSheet.create({
     fontSize: 24,
     fontWeight: "bold",
     marginBottom: 10,
+  },
+  extraInfo: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    padding: 10,
+  },
+  info: {
+    width: Dimensions.get("screen").width / 2.5,
+    backgroundColor: "#d0eafa",
+    padding: 10,
+    borderRadius: 10,
+    justifyContent: "space-around",
+  },
+  smallIcon: {
+    width: 40,
+    height: 40,
+    borderRadius: 20,
+    marginLeft: 50,
+  },
+  infoText: {
+    textAlign: "center",
+    fontSize: 18,
   },
 });
